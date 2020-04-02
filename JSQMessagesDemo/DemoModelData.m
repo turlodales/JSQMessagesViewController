40ef0ba054b725514bd504cfd97ed3681314108c
turlodales <paulturley_2@hotmail.com>
2020-04-02 08:36:03 +0100

    Update DemoModelData.m

--- JSQMessagesDemo/DemoModelData.m
+++ JSQMessagesDemo/DemoModelData.m
@@ -191,18 +191,3 @@
 }
 
 @end
-
-    © 2020 GitHub, Inc.
-    Terms
-    Privacy
-    Security
-    Status
-    Help
-
-    Contact GitHub
-    Pricing
-    API
-    Training
-    Blog
-    About
-
